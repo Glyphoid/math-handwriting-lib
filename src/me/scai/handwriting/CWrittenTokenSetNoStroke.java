8c367627d870c0aba5ef0f1b59535c9209248e5d
shanqing-cai <shanqing.cai@gmail.com>
2014-04-21 23:13:13 -0400

    Implemented readFromFile() of tokenSetNoStroke
    Also added a new main() test routine.

--- src/me/scai/handwriting/CWrittenTokenSetNoStroke.java
+++ src/me/scai/handwriting/CWrittenTokenSetNoStroke.java
@@ -1,6 +1,14 @@
 package me.scai.handwriting;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.lang.AssertionError;
+import java.io.FileNotFoundException;
+import java.io.InputStreamReader;
 
 public class CWrittenTokenSetNoStroke extends CAbstractWrittenTokenSet {
 	public ArrayList<float []> tokenBounds = new ArrayList<float []>();
@@ -14,6 +22,12 @@ public class CWrittenTokenSetNoStroke extends CAbstractWrittenTokenSet {
 		
 		if ( tokenNames.length != t_recogP.length ) {
 			System.err.println("Input t_recogP doesn't have the same length as tokenNames");
+			return;
+		}
+		
+		/* Make sure that t_recogWinner belongs to the set tokenNames */
+		if ( !Arrays.asList(tokenNames).contains(t_recogWinner) ) {
+			System.err.println("Value of t_recogWinner (" + t_recogWinner + ") does not belong to tokenNames");
 			return;
 		}
 		
@@ -115,8 +129,113 @@ public class CWrittenTokenSetNoStroke extends CAbstractWrittenTokenSet {
 	}
 	
 	/* Read from .wts file */
-	void readFromFile(String fileName) throws IOException {
+	void readFromFile(String fileName) throws FileNotFoundException, IOException {
+		File wtsFile = new File(fileName);
+		if ( !wtsFile.isFile() )
+			throw new FileNotFoundException("Cannot find file for reading: " + fileName);
 		
+		FileInputStream fin = null;
+		BufferedReader in = null;
+		try {			
+			fin = new FileInputStream(wtsFile);
+			in = new BufferedReader(new InputStreamReader(fin));
+		}
+		catch ( IOException e ) {
+			throw new IOException("IOException during reading of .wts file: " + fileName);
+		}
+		
+		String line;
+		String leadStr, trailStr, sepStr;		
+		try {									
+			/* Read token names */
+			leadStr = "Token set: [";
+			trailStr = "]";
+			sepStr = ", ";
+			
+			line = in.readLine();
+			assert(line.startsWith(leadStr));
+			assert(line.endsWith(trailStr));
+			line = line.substring(leadStr.length(), line.length() - trailStr.length());
+			
+			setTokenNames(line.split(sepStr));
+			in.readLine();
+			
+			/* Read tokens with no strokes, one by one */
+			while ( true ) {
+				line = in.readLine();
+				
+				if ( line == null || line.length() == 0 )
+					break;
+				
+				/* Bounds */
+				leadStr = "bounds = [";
+				trailStr = "]";
+				assert(line.startsWith(leadStr));
+				assert(line.endsWith(trailStr));
+				line = line.substring(leadStr.length(), line.length() - trailStr.length());
+				
+				String [] bndsStr = line.split(sepStr);
+				assert(bndsStr.length == 4);
+				float [] bnds = new float[4];
+				for (int i = 0; i < 4; ++i)
+					bnds[i] = Float.parseFloat(bndsStr[i]);
+				
+				/* recogWinner */
+				line = in.readLine();
+				leadStr = "recogWinner = ";
+				trailStr = "";
+				assert(line.startsWith(leadStr));
+				assert(line.endsWith(trailStr));				
+				String t_recogWinner = line.substring(leadStr.length(), line.length() - trailStr.length());
+				
+				/* recogPs */
+				line = in.readLine();
+				leadStr = "recogPs = [";
+				trailStr = "]";
+				assert(line.startsWith(leadStr));
+				assert(line.endsWith(trailStr));
+				line = line.substring(leadStr.length(), line.length() - trailStr.length());
+				
+				String [] psStr = line.split(sepStr);
+				assert(psStr.length == tokenNames.length);
+				double [] t_recogPs = new double[psStr.length];
+				for (int i = 0; i < psStr.length; ++i)
+					t_recogPs[i] = Double.parseDouble(psStr[i]);
+				
+				line = in.readLine();
+				
+				addToken(bnds, t_recogWinner, t_recogPs);
+			}
+			
+		}
+		catch ( AssertionError ae ) {
+			throw new IOException("Unexpected format in input .wts file: " + fileName);
+		}
+		catch ( IOException ioe ) {
+			throw new IOException("IOException occurred during reading input .wts file: " + fileName);
+		}
+		finally {
+			in.close();
+		}
+	}
+	
+	/* Testing routine */
+	public static void main(String [] args) {
+		/* Read .wts file */
+		final String wtsFileName = "C:\\Users\\systemxp\\Documents\\My Dropbox\\Plato\\data\\tokensets\\TS_1.wts";
+		CWrittenTokenSetNoStroke wt = new CWrittenTokenSetNoStroke();
+		
+		try {
+			wt.readFromFile(wtsFileName);
+		}
+		catch ( FileNotFoundException fnfe ) {
+			System.err.println(fnfe.getMessage());
+		}
+		catch ( IOException ioe ) {
+			System.err.println(ioe.getMessage());
+		}
+		
+		System.out.println("Done reading wts file: " + wtsFileName);
 	}
 	
 }
